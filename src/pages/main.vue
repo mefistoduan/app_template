bbe5d20142f596eff4ce7f0e1fc18bb94a26ce67
Changpeng Duan <mefistoduan@users.noreply.github.com>
2019-04-16 22:48:55 +0800

    416

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,9 +1,28 @@
 <template>
     <div id="pages">
-        <div class="container">
-            <div class="row">
-            main
-            </div>
+        <mt-swipe :auto="4000" :show-indicators="true">
+            <mt-swipe-item>
+                <div class="silder silder1"></div>
+            </mt-swipe-item>
+            <mt-swipe-item>
+                <div class="silder silder2"></div>
+            </mt-swipe-item>
+            <mt-swipe-item>
+                <div class="silder silder3"></div>
+            </mt-swipe-item>
+        </mt-swipe>
+        <div class="main_banner">
+            <div class="silder silder4"></div>
+        </div>
+        <div class="icon_list">
+            <ul>
+                <li>
+                    <a href="">
+                        <img src="" alt="">
+                        <em>xxx</em>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -12,6 +31,10 @@
     import axios from 'axios';
     let qs = require('qs');
     import Global from '../Global.js'
+    import { Swipe, SwipeItem } from 'mint-ui';
+    import Vue from 'vue'
+    Vue.component(Swipe.name, Swipe);
+    Vue.component(SwipeItem.name, SwipeItem);
 export default {
     data () {
     return {
@@ -33,4 +56,37 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     @import "../assets/css/comm.css";
+    #pages {
+        padding-bottom: 20px;
+    }
+    .silder{
+        width: 100%;
+        height: 220px;
+        overflow: hidden;
+        display: block;
+        margin: 0 auto;
+    }
+    .silder1 {
+        background: red;
+    }
+    .silder2 {
+        background: green;
+    }
+    .silder3 {
+        background: deepskyblue;
+    }
+    .silder4 {
+        background: yellow;
+    }
+    /deep/ .mint-swipe {
+        height: 230px;
+        overflow: hidden;
+    }
+    .main_banner {
+        width: 100%;
+        height: 100px;
+        overflow: hidden;
+        display: block;
+        margin: 0 auto;
+    }
 </style>
